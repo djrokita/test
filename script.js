b344a1976bd6bc95ad6bd82bdceb1e87e340edc7
Krzych <krzych.pierzchala@gmail.com>
2017-11-14 18:09:33 +0100

    Just for try

---
+++ script.js
@@ -0,0 +1,44 @@
+/*
+var movies = [{
+    id: 1,
+    title: 'Harry Potter',
+    desc: 'Film o małym czarodzieju',
+    img: 'http://1.fwcdn.pl/po/05/71/30571/7529392.2.jpg'
+  },
+  {
+    id: 2,
+    title: 'Król Lew',
+    desc: 'Historia króla sawanny',
+    img: 'http://1.fwcdn.pl/po/68/78/6878/6927221.2.jpg'
+  },
+  {
+    id: 3,
+    title: 'Oblivion',
+    desc: 'Postapokaliptyczna wizja świata',
+    img: 'http://1.fwcdn.pl/po/57/56/535756/7539332.2.jpg'
+  },
+  {
+    id: 4,
+    title: 'Prestige',
+    desc: 'Rywalizacja dwóch wielkich iluzjonistów',
+    img: 'http://1.fwcdn.pl/po/99/45/259945/7171824.2.jpg'
+  },
+  {
+    id: 5,
+    title: 'Perfume',
+    desc: 'Thriller w oświeceniowej Francji',
+    img: 'http://1.fwcdn.pl/po/84/42/118442/7096308.2.jpg'
+  },
+  {
+    id: 6,
+    title: 'Das Boot',
+    desc: 'Wojenne losy niemieckiego okrętu U-96',
+    img: 'http://1.fwcdn.pl/po/12/89/1289/7075142.2.jpg'
+  }
+];
+*/
+
+var element = <div><h1>Lista filmów</h1></div>
+
+
+ReactDOM.render(element, document.getElementById('app'));
